7ce2dcf7d4669ec111ad92971598ca7190bc4881
Jaskaran Kang <jaskarankang4@gmail.com>
2022-09-29 17:20:07 -0400

    Add project files.

---
+++ prog3050-game-store/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prog3050_game_store.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ prog3050-game-store/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prog3050_game_store.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
