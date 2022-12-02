7c76176f2326217d25ca38982ea819336c8927ef
srageh <ragehsuhaib@gmail.com>
2022-12-02 17:57:38 -0500

    merged

--- prog3050-game-store/Models/Merchandise.cs
+++ prog3050-game-store/Models/Merchandise.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GameStore.CustomValidation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Models
 {
@@ -11,11 +13,17 @@ namespace GameStore.Models
             WishlistItem = new HashSet<WishlistItem>();
         }
 
-        public int MerchandiseId { get; set; }
+        [Display(Name = "Game")]
         public int? GameId { get; set; }
+        [Required]
+        public int MerchandiseId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public float Price { get; set; }
+
 
         public Game Game { get; set; }
         public ICollection<CartMerchandise> CartMerchandise { get; set; }
